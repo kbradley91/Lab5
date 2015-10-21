8c76452b7b01614143eb696c63f73c473192b528
kbradley91 <kbradley09@gmail.com>
2015-10-21 11:09:07 -0400

    Lab5
    possible issue from sram portion changing linker file

---
+++ Debug/Lab5_PART2.pp
@@ -0,0 +1,61 @@
+# FIXED
+
+Lab5_PART2.obj: ../Lab5_PART2.c
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP28x_Project.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Examples.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_ePwm_defines.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Dma_defines.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_I2C_defines.h
+Lab5_PART2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_DefaultISR.h
+Lab5_PART2.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/string.h
+Lab5_PART2.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/linkage.h
+
+../Lab5_PART2.c: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP28x_Project.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Examples.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_ePwm_defines.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Dma_defines.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_I2C_defines.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_DefaultISR.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/string.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/linkage.h: 
